cc34dc31a4115be7eca6e2ebb340b8fbd00cff7a
ronaldcc25 <racalderon13@utpl.edu.ec>
2024-05-16 15:39:33 -0500

    Listo

--- EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/EjemploCuatro.java
+++ EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/EjemploCuatro.java
@@ -9,37 +9,38 @@ import paquete01.Calificacion;
 import paquete01.Profesor;
 
 public class EjemploCuatro {
+
     public static void main(String[] args) {
         /*
         Los objetos(clases) se puedes usar dentro de otros obtjetos
         y se los usaria al momento de presentar como esta en la linea 35
-        */
+         */
         Profesor profesor1 = new Profesor("Richard", "nombramiento");
         Profesor profesor2 = new Profesor("Rene", "contrato");
-        
+
         // crear un arreglo de objetos de tipo Calificacion
-        Calificacion [] calificaciones = new Calificacion[2];
-        
-        Calificacion c = new Calificacion(10, "Computación",profesor1);
-        Calificacion c2 = new Calificacion(9, "Electrónica",profesor2);
-        
+        Calificacion[] calificaciones = new Calificacion[2];
+
+        Calificacion c = new Calificacion(10, "Computación", profesor1);
+        Calificacion c2 = new Calificacion(9, "Electrónica", profesor2);
+
         calificaciones[0] = c;
         calificaciones[1] = c2;
-        
+
         for (int i = 0; i < calificaciones.length; i++) {
             Calificacion objetoCalificacion = calificaciones[i];
-            System.out.printf("%s - %.2f - Profesor(%s - %s)\n", 
+            System.out.printf("%s - %.2f - Profesor(%s - %s)\n",
                     objetoCalificacion.obtenerNombreMateria(),
-                    objetoCalificacion.obtenerNota(), 
+                    objetoCalificacion.obtenerNota(),
                     /*
                     Para presentar los datos del objeto (clase) llamada Profesor
                     tenemos que ingresar por medio del nuevo objeto creado en
                     la linea 30 a los metodos para poder presentar los nombres
                     y los tipos
-                    */
+                     */
                     objetoCalificacion.obtenerProfesor().obtenerNombre(),
-                    objetoCalificacion.obtenerProfesor().obtenerTipo());            
+                    objetoCalificacion.obtenerProfesor().obtenerTipo());
         }
-        
+
     }
 }
--- EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/EjemploTres.java
+++ EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/EjemploTres.java
@@ -8,17 +8,24 @@ package paquete03;
 import paquete01.Calificacion;
 
 public class EjemploTres {
+
     public static void main(String[] args) {
         // crear un arreglo de objetos de tipo Calificacion
-        Calificacion [] calificaciones = {new Calificacion(10, "Computación"), 
+        Calificacion[] calificaciones = {
+            new Calificacion(10, "Computación"),
             new Calificacion(9, "Electrónica")};
-        
+        /*
+        Hay como darle valor a un arreglo de tipo Calificacion(clase) para no
+        tener que crear los objetos aparte y su sintaxis para dar valor seria
+        new (nombre del tipo de la variable o el nombre de la clase) (atributos)
+        */
+
         for (int i = 0; i < calificaciones.length; i++) {
             Calificacion objetoCalificacion = calificaciones[i];
-            System.out.printf("%s - %.2f\n", 
+            System.out.printf("%s - %.2f\n",
                     objetoCalificacion.obtenerNombreMateria(),
                     objetoCalificacion.obtenerNota());
         }
-        
+
     }
 }
